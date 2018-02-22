e4934d0997f57869c7724737e96c877779b18e82
Harish Chouhan <hello@dreamsmedia.in>
2018-02-22 18:49:08 +0700

    Change ABSPATH to WPINC

--- custom-login-logo.php
+++ custom-login-logo.php
@@ -27,7 +27,11 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
 if ( ! class_exists( 'DOT_CLL' ) ) {
 
 
